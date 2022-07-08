2d56676f07a29b13e07ac5520749f99071ee5496
gespyrop <giwrgosspyropoulos@gmail.com>
2022-07-09 01:59:38 +0300

    MFCC features for all audio files in a directory

--- utils.py
+++ utils.py
@@ -1,26 +1,92 @@
 import os
 import librosa
+from tqdm import tqdm
 import numpy as np
+import pandas as pd
 
 
-def load_dataset(directory):
+def load_dataset(directory: str) -> pd.DataFrame:
+    '''
+    load_dataset(directory='data')
+
+    Parses all the audio files of a directory into a DataFrame.
+
+    Parameters
+    ----------
+    directory : str
+        The directory containing the audio files.
+
+    Returns
+    -------
+    data : pd.DataFrame
+        The parsed DataFrame containing data
+        for all the audio files in the directory.
+    '''
     files = os.listdir(directory)
 
-    features = list(
-        map(lambda f: get_features_from_file(directory, f), files)
+    data = pd.DataFrame(
+        columns=['filename', 'sample_index', 'features', 'label']
     )
 
-    labels = list(
-        map(lambda f: f.split('_')[0], files)
-    )
+    print("Loading dataset...")
+    for f in tqdm(files):
+        data = pd.concat(
+            (data, get_data_from_file(directory, f)),
+            ignore_index=True
+        )
 
-    return features, labels
+    return data
 
 
-def get_features_from_file(directory, f):
+def get_data_from_file(directory: str, f: str) -> pd.DataFrame:
+    '''
+    get_data_from_file(directory='data', f='1_george_0.wav')
+
+    Reads a file into a DataFrame containing the filename, sample_index,
+    MFCC features and label of each sample of the audio.
+
+    Parameters
+    ----------
+    directory : str
+        The directory containing the audio files.
+    f : str
+        The name of the file
+
+    Returns
+    -------
+    data : pd.DataFrame
+        The parsed DataFrame containing data for the audio in the file.
+    '''
     path = os.path.join(directory, f)
-    signal = librosa.load(path)[0]
-    return librosa.feature.mfcc(y=signal)
+    signal, sr = librosa.load(path)
+
+    data = pd.DataFrame(
+        columns=['filename', 'sample_index', 'features', 'label']
+    )
+
+    samples = get_equal_samples(signal, sr)
+
+    for sample_index in range(samples.shape[0]):
+        sample = samples[sample_index]
+
+        # MFCC features
+        features = librosa.feature.mfcc(y=sample, n_mfcc=13)
+
+        # 1st derivative features
+        delta_features = librosa.feature.delta(features)
+
+        # 2nd detivative features
+        delta2_features = librosa.feature.delta(features, order=2)
+        features = np.concatenate((features, delta_features, delta2_features))
+
+        # Final feature vector
+        features = features.reshape(-1)
+
+        label = f.split('_')[0] if sample_index == 0 else None
+
+        data.loc[len(data.index)] = [f, sample_index, features, label]
+
+    return data
 
 
 def get_equal_samples(y: np.ndarray, sr: int, duration: int = 1) -> np.ndarray:
@@ -36,8 +102,8 @@ def get_equal_samples(y: np.ndarray, sr: int, duration: int = 1) -> np.ndarray:
         The initial signal.
     sr : int
         The sample rate.
-    duration : int
-        The duration of each sample in seconds.
+    duration : int, optional
+        The duration of each sample in seconds. Default is 1.
 
     Returns
     -------
